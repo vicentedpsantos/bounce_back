896c8e64a9c799ec2471f26a4989373ecacd38a9
Vicente Santos <vicentedpsantos@gmail.com>
2024-11-23 18:31:53 +0000

    Add test to main module

--- test/bounce_back_test.exs
+++ test/bounce_back_test.exs
@@ -1,8 +1,32 @@
 defmodule BounceBackTest do
   use ExUnit.Case
-  doctest BounceBack
+  import BounceBack
 
-  test "greets the world" do
-    assert BounceBack.hello() == :world
+  test "retries a block until success" do
+    success_after = 3
+    call_count = :erlang.make_ref()
+    Process.put(call_count, 0)
+
+    retry max_retries: 5 do
+      count = Process.get(call_count)
+      Process.put(call_count, count + 1)
+
+      if count >= success_after do
+        {:ok, "Success"}
+      else
+        {:error, "Failure"}
+      end
+    end
+
+    assert Process.get(call_count) == success_after + 1
+  end
+
+  test "returns error if retries are exhausted" do
+    result =
+      retry max_retries: 3 do
+        {:error, "Always fails"}
+      end
+
+    assert result == {:error, "Always fails"}
   end
 end
