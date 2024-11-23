bb1d2444cb85c4f51c5ef256d69f6042345abf69
Vicente Santos <vicentedpsantos@gmail.com>
2024-11-23 19:58:34 +0000

    Bump to 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BounceBack.MixProject do
   def project do
     [
       app: :bounce_back,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
